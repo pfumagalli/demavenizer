edfc6e5e92b8e35ba139776ae9b65fcdcae2e5dc
Pier Fumagalli <ianosh@gmail.com>
2013-07-16 02:18:44 +0900

    Use proper library

--- demavenizer.sh
+++ demavenizer.sh
@@ -2,7 +2,7 @@
 
 java -cp target/eclipse \
      -Dmaven.url=http://repo1.maven.org/maven2/ \
-     -Dlibraries.dir=./libraries \
-     -Dmappings.file=./libraries/mappings.properties \
-     -Dlicenses.file=./libraries/licenses.properties \
+     -Dlibraries.dir=../libraries \
+     -Dmappings.file=../libraries/mappings.properties \
+     -Dlicenses.file=../libraries/licenses.properties \
      com.github.pfumagalli.demavenizer.Main "${@}"
